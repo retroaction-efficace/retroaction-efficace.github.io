a36e768f5efe07f00fc286a5bbbf17956f5d82c1
retroaction-efficace <catherine.theberge.2@gmail.com>
2024-09-30 20:30:24 -0400

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,3 @@
-fetch('components/header.html')
-  .then(response => response.text())
-  .then(data => document.querySelector('header').innerHTML = data);
 fetch('components/footer.html')
   .then(response => response.text())
   .then(data => document.querySelector('footer').innerHTML = data);
