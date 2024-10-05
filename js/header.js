b55fa4a67996e69d5e341e3d499e7da94dda1545
retroaction-efficace <catherine.theberge.2@gmail.com>
2024-10-05 12:11:51 -0400

    Update header.js

--- js/header.js
+++ js/header.js
@@ -4,17 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             document.querySelector('header').innerHTML = data;
 
+            // Add event listeners after loading the header
             document.querySelector('.menu').addEventListener('click', () => {
                 const nav = document.querySelector('.nav');
+                const main = document.querySelector('main');
                 if (nav.style.width === '250px') {
                     nav.style.width = '0';
+                    main.style.marginRight = '0';
                 } else {
                     nav.style.width = '250px';
+                    main.style.marginRight = '250px';
                 }
             });
 
             document.querySelector('.nav').addEventListener('mouseleave', function() {
                 this.style.width = '0';
+                document.querySelector('main').style.marginRight = '0';
             });
         });
 });
