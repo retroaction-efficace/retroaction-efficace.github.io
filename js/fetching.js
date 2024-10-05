c211a9e44c3a5616f289afe081a9183ec9a9361c
retroaction-efficace <catherine.theberge.2@gmail.com>
2024-10-05 18:37:56 -0400

    Update fetching.js

--- js/fetching.js
+++ js/fetching.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-            fetch('components/fetching.html')
+            fetch('components/header.html')
                 .then(response => response.text())
                 .then(data => {
                     document.querySelector('header').innerHTML = data;
