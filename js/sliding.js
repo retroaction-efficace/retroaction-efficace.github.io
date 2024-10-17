596bf711e3d2683fd94af0fc16b088dd9be52a15
retroaction-efficace <catherine.theberge.2@gmail.com>
2024-10-16 20:29:51 -0400

    Update sliding.js

--- js/sliding.js
+++ js/sliding.js
@@ -7,14 +7,18 @@ function plusSlides(n) {
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("slide");
+    let indicators = document.getElementsByClassName("slide-indicator")[0];
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
     slides[slideIndex-1].style.display = "grid";
+
+    indicators.innerHTML = '';
+    for (let i = 0; i < slides.length; i++) {
+        indicators.innerHTML += (i <= slideIndex-1) ? '&#x25CF; ' : '&#x25CB; ';
+    }
 }
 
 document.querySelector('.left').addEventListener('click', () => plusSlides(-1));
